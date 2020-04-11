0e3cafea93cab2de42ed27518b78c4ce39fea4c9
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-04-11 15:03:51 +0200

    feat: delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -133,4 +133,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route     DELETE api/profile
+// @desc      Delete profile, user and posts
+// @access    Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // TODO remove users posts
+    // Remove user profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove the user
+    await User.findByIdAndRemove({ _id: req.user.id });
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Service Error");
+  }
+});
+
 module.exports = router;
