ef0d98c00b42725b172256b940804caa7a1d84de
Ralence <ranko.ostojic@gmail.com>
2020-05-09 19:33:49 +0200

    feat: add private route for dashboard

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !isAuthenticated && !loading ? <Redirect to="/login" /> : <Component {...props} />
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
