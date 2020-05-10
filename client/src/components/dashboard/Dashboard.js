9928a42d14c2a78f1da368fd3462c85326797f00
Ralence <ranko.ostojic@gmail.com>
2020-05-10 11:37:20 +0200

    feat: add profile reducer and get profile action

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getCurrentProfile } from "../../actions/profile";
 
 const Dashboard = () => {
+  const { auth, profile } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentProfile());
+  }, []);
+
   return <div>DAshboard</div>;
 };
 
