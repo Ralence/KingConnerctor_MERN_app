f4a7ff01ee30efa04f2d455f77b87ebeab26a5c0
Ralence <ranko.ostojic@gmail.com>
2020-05-12 17:49:02 +0200

    feat: add ceratProfile component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -33,7 +33,7 @@ const Dashboard = () => {
       ) : (
         <Fragment>
           <p>You have not yet set up a profile, please add info!</p>
-          <Link to="/creat-profile" className="btn btn-primary my-1">
+          <Link to="/create-profile" className="btn btn-primary my-1">
             Create Profile
           </Link>
         </Fragment>
