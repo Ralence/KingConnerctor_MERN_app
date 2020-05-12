efd2b5e437977f442c0f9e72694d42cb179c36c0
Ralence <ranko.ostojic@gmail.com>
2020-05-12 08:06:23 +0200

    feat: start on creatin the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import Spinner from "../layout/Spinner";
+
 import { getCurrentProfile } from "../../actions/profile";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  const { auth, profile } = useSelector((state) => state);
+  const {
+    auth: { user },
+    profile: { profile, loading },
+  } = useSelector((state) => state);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCurrentProfile());
   }, []);
 
-  return <div>DAshboard</div>;
+  if (loading && profile === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet set up a profile, please add info!</p>
+          <Link to="/creat-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 export default Dashboard;
