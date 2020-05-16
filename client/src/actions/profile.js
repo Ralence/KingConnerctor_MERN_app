3bd4eef6c96f6078704f98929976343451b8c4da
Ralence <ranko.ostojic@gmail.com>
2020-05-16 18:10:16 +0200

    feat: addEducation action set up

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -101,3 +101,41 @@ export const addExperience = (formData, history) => async (dispatch) => {
     });
   }
 };
+
+// Add education
+export const addEducation = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.put("/api/profile/education", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(`Education Added`, "success"));
+
+    history.push("/dashboard");
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach((err) => {
+        dispatch(setAlert(err.msg, "danger"));
+      });
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
