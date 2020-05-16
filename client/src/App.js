b44a193c2812160832a0ead395df58ea92de7db1
Ralence <ranko.ostojic@gmail.com>
2020-05-16 18:56:01 +0200

    feat: set up AddExperience form and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
+import AddExperience from "./components/profile-form/AddExperience";
 
 import PrivateRoute from "./components/routing/PrivateRoute";
 
@@ -43,6 +44,7 @@ const App = () => {
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+              <PrivateRoute exact path="/add-experience" component={AddExperience} />
             </Switch>
           </section>
         </Fragment>
