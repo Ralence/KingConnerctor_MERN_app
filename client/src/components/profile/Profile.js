87bf15f2281736324c363acce0986159cd2e434d
Ralence <ranko.ostojic@gmail.com>
2020-05-24 21:09:05 +0200

    feat: add github repos to the profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({ match: { params } }) => {
@@ -59,6 +60,8 @@ const Profile = ({ match: { params } }) => {
                 <h4>No Education Credentials</h4>
               )}
             </div>
+
+            {profile.githubusername && <ProfileGithub username={profile.githubusername} />}
           </div>
         </Fragment>
       )}
